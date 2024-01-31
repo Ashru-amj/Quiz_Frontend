b948b534e6d77ce635c69ee4a3e2776e4a4f3822
jenaamj <jenaamj@gmail.com>
2024-01-31 22:56:43 +0530

    url2

--- src/config.js
+++ src/config.js
@@ -4,4 +4,4 @@ export const BACKEND_URL = "https://quizzie-gxj9.onrender.com/api";
 //export const FRONTEND_URL = "https://localhost:3000";
 //export const BACKEND_URL = "https://localhost:4000";
 //export const FRONTEND_URL = "https://65ba586c458e03e68f8e3b03--boisterous-cajeta-1e9041.netlify.app";
-export const FRONTEND_URL = "https://boisterous-cajeta-1e9041.netlify.app"
+export const FRONTEND_URL = "https://elegant-gaufre-317719.netlify.app"
